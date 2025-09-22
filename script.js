53a0f1611c5aad0f21249a9cc1b6deb2d5fccf2f
Rafael Bueno Gargioni <rbgargioni2@gmail.com>
2025-09-22 19:58:26 -0300

    Alterando

--- script.js
+++ script.js
@@ -180,5 +180,19 @@ toggleBtn.addEventListener('click', () => {
 });
 
 
+document.addEventListener("DOMContentLoaded", () => {
+  const images = document.querySelectorAll(".carousel img");
+  let index = 0;
+
+  if (images.length > 0) {
+    images[index].classList.add("active");
+
+    setInterval(() => {
+      images[index].classList.remove("active");
+      index = (index + 1) % images.length;
+      images[index].classList.add("active");
+    }, 5000);
+  }
+});
 
 
